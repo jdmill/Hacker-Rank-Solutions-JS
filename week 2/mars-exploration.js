b5317cd11ebb6dcdc9bf557770ae25abda697fcd
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-05-11 19:45:28 -0400

    adding a few solutions

---
+++ week 2/mars-exploration.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'marsExploration' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+function marsExploration(s) {
+  // Write your code here
+  let mod = 0;
+  let arr = s.split("");
+
+  for (let i = 0; i < arr.length; i += 3) {
+    if (arr[i] !== "S") {
+      mod++;
+    }
+    if (arr[i + 1] !== "O") {
+      mod++;
+    }
+    if (arr[i + 2] !== "S") {
+      mod++;
+    }
+  }
+  return mod;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const s = readLine();
+
+  const result = marsExploration(s);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}

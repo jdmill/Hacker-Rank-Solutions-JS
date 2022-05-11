523ffa74db564340a35bff95de7ec5a2343027df
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-05-10 23:51:42 -0400

    added a few more solutions

---
+++ week 2/counting-valleys.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+function countingValleys(steps, path) {
+  // Write your code here
+  let elevation = 0;
+  let prevEl = 0;
+  let vCount = 0;
+
+  for (let i = 0; i < steps; i++) {
+    if (path[i] === "U") {
+      prevEl = elevation;
+      elevation++;
+    } else {
+      prevEl = elevation;
+      elevation--;
+    }
+
+    if (prevEl === -1 && elevation === 0) {
+      vCount++;
+    }
+  }
+  return vCount;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const steps = parseInt(readLine().trim(), 10);
+
+  const path = readLine();
+
+  const result = countingValleys(steps, path);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}

e581a8ad2fda457d5590137cae92d258ca00c3af
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-06-11 12:11:15 -0400

    added hacker rank solution

---
+++ week 4/pickingNumbers.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pickingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function pickingNumbers(a) {
+  a.sort((a, b) => a - b);
+  console.log(a);
+  let longest = 0;
+
+  for (let i = 0; i < a.length; i++) {
+    let length = 0;
+    for (let j = i; j < a.length; j++) {
+      if (Math.abs(a[j]) - Math.abs(a[i]) <= 1) {
+        length++;
+      }
+    }
+    if (length > longest) {
+      longest = length;
+    }
+  }
+  console.log(longest);
+  return longest;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const a = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((aTemp) => parseInt(aTemp, 10));
+
+  const result = pickingNumbers(a);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
